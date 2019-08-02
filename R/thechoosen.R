bb16a7e33a1051d628767975b2537c5d252e88c3
Thomas <thma@sund.ku.dk>
2019-08-02 13:46:04 +0200

    Update R/thechoosen.R

--- R/thechoosen.R
+++ R/thechoosen.R
@@ -2,7 +2,6 @@ thechoosen<-function(x){
 	set.seed(as.numeric(ISOdate(as.numeric(format(Sys.time(), "%Y")),
                            as.numeric(format(Sys.time(), "%m")),
                            as.numeric(format(Sys.time(), "%d")))))
-    outper<-sample(x,1)
-    out<-do.call("Nameperson",list(x=outper))
+    out<-sample(x,1)
     return((out))
 }
