2ec264d960925e3b9cfd3ebabe8856b90113c16f
Thomas Maltesen <thma@sund.ku.dk>
2019-08-09 15:51:41 +0200

    Update sampleset.R

--- R/sampleset.R
+++ R/sampleset.R
@@ -1,5 +1,5 @@
 sampleset<-function(x=NULL){
-	nameslist<-c("Anne","Jacob","Kenneth","Laure","Martin","Michael","Thomas")
+	nameslist<-c("A","J","K","Thomas")
 	out<-list()
 	out$possible<-if(is.null(x))nameslist else nameslist[!(tolower(nameslist) %in% tolower(x))]
 	out$nameslist<-nameslist
